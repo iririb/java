ae1b04dffa9d2d8118462f39e9feb4d1a6160c7f
iruiru <eshamibrahim94@gmail.com>
2022-11-15 16:57:41 +0800

    Class loader hierarchy

--- src/main/java/test/keyword/App.java
+++ src/main/java/test/keyword/App.java
@@ -1,29 +1,21 @@
 package test.keyword;
 
-import java.net.http.HttpClient;
 import java.sql.ResultSet;
-
-import java.util.ArrayList;
-
-import org.json.JSONObject;
 
 public class App {
 	public static void main(String[] args) {
 
-		// this will load from app class loader
-		System.out.println("Class loader: " + Employee.class.getClassLoader());
+		ClassLoader bootStrapClassLoader = String.class.getClassLoader();
+		System.out.println(bootStrapClassLoader);
 		
-		// these will load from platform class loader
-		System.out.println("Class loader: " + ResultSet.class.getClassLoader());
-		System.out.println("Class loader: " + HttpClient.class.getClassLoader());
+		ClassLoader platformClassLoader = ResultSet.class.getClassLoader();
+		System.out.println(platformClassLoader);
+		System.out.println(platformClassLoader.getParent());
 		
-		// this will load bootstrap class loader
-		System.out.println("Class loader: " + ArrayList.class.getClassLoader());
-		// however the result will be null
-		
-		// here we readng the class loader of imported jar file(Refer Referenced Library folder)
-		// this will also load from app class loader
-		System.out.println("Class loader: " + JSONObject.class.getClassLoader());
+		ClassLoader appClassLoader = Employee.class.getClassLoader();
+		System.out.println(appClassLoader);
+		System.out.println(appClassLoader.getParent());
+		System.out.println(appClassLoader.getParent().getParent());
 		
 	}
 }
