07d54081f4295bb4e0a6d454e9fbae749129cccc
iruiru <eshamibrahim94@gmail.com>
2022-11-15 15:18:22 +0800

    Redefine default method

--- src/main/java/test/keyword/App.java
+++ src/main/java/test/keyword/App.java
@@ -3,9 +3,20 @@ package test.keyword;
 public class App {
 	public static void main(String[] args) {
 
-		Automobile huracan = new Lamborghini("Huracan", 1200000.0, 320);
+		Automobile huracan = new Lamborghini("Huracan", 1200000.0, 320, "2014");
 		System.out.println(huracan);
-		System.out.println(huracan.getReleaseDate());
+		
+		// if we change the object to SportsCar will cause error as getReleaseDate is from Automobile
+		// System.out.println(huracan.getReleaseDate());
 
+		// changing back the object to Automobile to test for override default method
+		System.out.println(huracan.getReleaseDate());
+		
+		Automobile sedan = new Sedan("Honda", "Civic", 77000.0);
+		System.out.println(sedan);
+		// here the get release will use the default method as it was not redefined in the Sedan class
+		System.out.println(sedan.getReleaseDate());
+		
+		
 	}
 }
--- src/main/java/test/keyword/Lamborghini.java
+++ src/main/java/test/keyword/Lamborghini.java
@@ -7,12 +7,20 @@ public class Lamborghini implements SportsCar, Automobile{
 	private String model;
 	private Double price;
 	private float topSpeed;
+	private String releaseDate;
 	
-	public Lamborghini(String model, Double price, float topSpeed) {
+	public Lamborghini(String model, Double price, float topSpeed, String releaseDate) {
 		this.make = "Lamborghini";
 		this.model = model;
 		this.price = price;
 		this.topSpeed = topSpeed;
+		this.releaseDate = releaseDate;
+	}
+	
+	// if we dont want to depend on default method we can redefine here
+	@Override
+	public String getReleaseDate() {
+		return releaseDate;
 	}
 	
 	@Override
