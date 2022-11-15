1d09b4507a41fcc57d011117f885487f0bd17bd9
iruiru <eshamibrahim94@gmail.com>
2022-11-15 16:51:05 +0800

    Class loader of imported jar

--- src/main/java/test/keyword/App.java
+++ src/main/java/test/keyword/App.java
@@ -5,6 +5,8 @@ import java.sql.ResultSet;
 
 import java.util.ArrayList;
 
+import org.json.JSONObject;
+
 public class App {
 	public static void main(String[] args) {
 
@@ -18,6 +20,10 @@ public class App {
 		// this will load bootstrap class loader
 		System.out.println("Class loader: " + ArrayList.class.getClassLoader());
 		// however the result will be null
+		
+		// here we readng the class loader of imported jar file(Refer Referenced Library folder)
+		// this will also load from app class loader
+		System.out.println("Class loader: " + JSONObject.class.getClassLoader());
 		
 	}
 }
