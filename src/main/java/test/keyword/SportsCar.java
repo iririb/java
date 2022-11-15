1eeb214b660929a58078374c7f522c631b2cfab5
iruiru <eshamibrahim94@gmail.com>
2022-11-15 13:18:00 +0800

    Create a same method between 2 interfaces

--- src/main/java/test/keyword/SportsCar.java
+++ src/main/java/test/keyword/SportsCar.java
@@ -2,4 +2,10 @@ package test.keyword;
 
 public interface SportsCar{
 	float getTopSpeed();
+	
+	// add method similar to Automobile
+	String getModel();
+	
+	// however is we have another similar method with Automobile but different in type will cause an error
+	Integer getPrice();
 }
